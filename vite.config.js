766e43941c74c1509ef7cf966bd15e6c195fa5b1
Neatsara Somnuek <neatsara.b@gmail.com>
2026-03-27 14:38:03 +0700

    update viteDevtool

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
-import { fileURLToPath, URL } from 'node:url'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
+import { fileURLToPath, URL } from "node:url";
+// --- ใช้บรรทัดข้างล่าง เพื่อเปิด vueDevTool --- //
+import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true }),
-    vueDevTools(),
-  ],
+  // plugins: [vue(), vuetify({ autoImport: true })],
+  // --- เปิดบรรทัดข้างล่าง และ ปิดข้างบน เพื่อเปิด vueDevTool --- //
+  plugins: [vue(), vuetify({ autoImport: true }), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
